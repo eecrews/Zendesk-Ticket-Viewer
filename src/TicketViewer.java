219d5d63fe480c16c47ae4a8434a188e44ac711c
eecrews <61025723+eecrews@users.noreply.github.com>
2021-11-25 15:59:15 -0600

    update pagePartition
    currently does not account for # tickets that are not evenly divisible by 25

--- src/TicketViewer.java
+++ src/TicketViewer.java
@@ -1,45 +1,33 @@
 import java.lang.ProcessBuilder;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class TicketViewer {
 	
 	private static ArrayList<String> tickets = new ArrayList<String>();
-	private static ArrayList<ArrayList<String>> ticketPages = new ArrayList<ArrayList<String>>();
+	private static ArrayList<List<String>> ticketPages = new ArrayList<List<String>>();
 	private static int numTickets;
 	private static int numPages = 1;
 	
 	private static void pagePartition() {
 		for(int i=1; i<=numPages; i++) {
-			ArrayList<String> newPage = new ArrayList<String>();
+			List<String> newPage;
 
-			for(int j=0; j<25; j++) {
-				if(tickets.get(j) != null)
-					newPage.add(tickets.remove(j));
-			}
+			newPage = tickets.subList((i-1)*25, i*25);
 
 			ticketPages.add(newPage);
 		}
 	}
 	
 	private static String viewAllTickets() {
-		String ticketsOnPage = "";
-		int startIndex = (pageNum-1)*25;
-		int endIndex = (pageNum*25);
+		String allTickets = "";
 		
-		for(int i=startIndex; i<endIndex; i=i+5) {
-			if(tickets.get(i) == null)
-				break;
-			
-			for(int j=i; j<=i+5; j++) {
-				ticketsOnPage = ticketsOnPage + tickets.get(j) + "\t";
-			}
-			
+		for(int i=0; i<numPages; i++) {
+			allTickets += ticketPages.get(i).toString() + "\n";
 		}
-		
-		ticketsOnPage = Arrays.toString(tickets.toArray());
 
-		return ticketsOnPage;
+		return allTickets;
 	}
 	
 	private static void driver() {
@@ -48,13 +36,10 @@ public class TicketViewer {
 		// Updating numPages 
 		if(tickets.size() > 25) {
 			numPages = (int)Math.ceil(tickets.size()/25.0);
-			System.out.println(numPages);
 		}
 		
-		for(int i=1; i<=numPages; i++) {
-			System.out.println(viewPage(i));
-			System.out.println("-----");
-		}
+		pagePartition();
+		System.out.println(viewAllTickets());
 		
 	}
 	
@@ -87,8 +72,33 @@ public class TicketViewer {
 		tickets.add("Two");
 		tickets.add("Three");
 		tickets.add("One");
+		
+		tickets.add("One");
 		tickets.add("Two");
 		tickets.add("Three");
+		tickets.add("One");
+		tickets.add("Two");
+		tickets.add("Three");
+		tickets.add("One");
+		tickets.add("Two");
+		tickets.add("Three");
+		tickets.add("One");
+		tickets.add("Two");
+		tickets.add("Three");
+		tickets.add("One");
+		tickets.add("Two");
+		tickets.add("Three");
+		tickets.add("One");
+		tickets.add("Two");
+		tickets.add("Three");
+		tickets.add("One");
+		tickets.add("Two");
+		tickets.add("Three");
+		tickets.add("One");
+		tickets.add("Two");
+		tickets.add("Three");
+		tickets.add("One");
+		
 		
 		driver();
 
