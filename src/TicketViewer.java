43c13b38972b582b1b36a8be7b17448afe4324a7
eecrews <61025723+eecrews@users.noreply.github.com>
2021-11-25 14:41:38 -0600

    add viewAllTickets() and main()

---
+++ src/TicketViewer.java
@@ -0,0 +1,64 @@
+import java.lang.ProcessBuilder;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class TicketViewer {
+	
+	private static ArrayList<String> tickets = new ArrayList<String>();
+	
+	private static String viewAllTickets() {
+		String allTickets = "";
+		
+		allTickets = Arrays.toString(tickets.toArray());
+		
+		int numPages = 1; // Number of pages shown to user is 1 by default.
+		
+		if(tickets.size() > 25) {
+			numPages = (int)Math.ceil(tickets.size()/25.0);
+		}
+		
+		for(int i=0; i<numPages; i++) {
+			
+		}
+
+		return allTickets;
+	}
+	
+
+	
+	public static void main(String[] args) {
+		
+		tickets.add("One");
+		tickets.add("Two");
+		tickets.add("Three");
+		tickets.add("One");
+		tickets.add("Two");
+		tickets.add("Three");
+		tickets.add("One");
+		tickets.add("Two");
+		tickets.add("Three");
+		tickets.add("One");
+		tickets.add("Two");
+		tickets.add("Three");
+		tickets.add("One");
+		tickets.add("Two");
+		tickets.add("Three");
+		tickets.add("One");
+		tickets.add("Two");
+		tickets.add("Three");
+		tickets.add("One");
+		tickets.add("Two");
+		tickets.add("Three");
+		tickets.add("One");
+		tickets.add("Two");
+		tickets.add("Three");
+		tickets.add("One");
+		tickets.add("Two");
+		tickets.add("Three");
+		
+		System.out.println(viewAllTickets());
+
+	}
+	
+
+}
